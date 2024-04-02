87d60774774c93234e045f4026ef63e3b1e75cff
Gusty <a0985209465@gmail.com>
2024-04-03 05:13:38 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://gusty1.github.io/running_page/',
+  siteUrl: 'https://running-page-git-master-gusty1.vercel.app/',
   logo: 'https://github.com/Gusty1/GustyLittleWorld/blob/gh-pages/images/index/icon-rm.png?raw=true',
   description: 'Personal site and blog',
   navLinks: [
