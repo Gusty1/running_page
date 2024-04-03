13e036c8dc1d2db5158a66c634cef25164e63682
Gusty <a0985209465@gmail.com>
2024-04-03 17:27:19 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-   'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+   'sk.eyJ1IjoiZ3VzdHl5IiwiYSI6ImNsdWpsc2ZrbjBjOGkyaWxpcXh1dHlka2kifQ.7sX_1lFXxRnhSEaso2MLZQ';
 const MUNICIPALITY_CITIES_ARR = [
    '北京市',
    '上海市',
