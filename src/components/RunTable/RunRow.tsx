de69124625b5b81885ce567fc93b2a92e4f896a7
Gusty <a0985209465@gmail.com>
2024-06-08 00:05:38 +0800

    rowTable 排版更新

--- src/components/RunTable/RunRow.tsx
+++ src/components/RunTable/RunRow.tsx
@@ -1,46 +1,60 @@
-import { formatPace, titleForRun, formatRunTime, Activity, RunIds } from '@/utils/utils';
+import {
+    formatPace,
+    titleForRun,
+    formatRunTime,
+    Activity,
+    RunIds,
+} from '@/utils/utils';
 import styles from './style.module.css';
 
 interface IRunRowProperties {
-  elementIndex: number;
-  locateActivity: (_runIds: RunIds) => void;
-  run: Activity;
-  runIndex: number;
-  setRunIndex: (_ndex: number) => void;
+    elementIndex: number;
+    locateActivity: (_runIds: RunIds) => void;
+    run: Activity;
+    runIndex: number;
+    setRunIndex: (_ndex: number) => void;
 }
 
-const RunRow = ({ elementIndex, locateActivity, run, runIndex, setRunIndex }: IRunRowProperties) => {
-  const distance = (run.distance / 1000.0).toFixed(2);
-  const paceParts = run.average_speed ? formatPace(run.average_speed) : null;
-  const heartRate = run.average_heartrate;
-  const runTime = formatRunTime(run.moving_time);
-  const handleClick = () => {
-    if (runIndex === elementIndex) {
-      setRunIndex(-1);
-      locateActivity([]);
-      return
+const RunRow = ({
+    elementIndex,
+    locateActivity,
+    run,
+    runIndex,
+    setRunIndex,
+}: IRunRowProperties) => {
+    const distance = (run.distance / 1000.0).toFixed(2);
+    const paceParts = run.average_speed ? formatPace(run.average_speed) : null;
+    const heartRate = run.average_heartrate;
+    const runTime = formatRunTime(run.moving_time);
+    const handleClick = () => {
+        if (runIndex === elementIndex) {
+            setRunIndex(-1);
+            locateActivity([]);
+            return;
+        }
+        setRunIndex(elementIndex);
+        locateActivity([run.run_id]);
     };
-    setRunIndex(elementIndex);
-    locateActivity([run.run_id]);
-  };
 
-  return (
-    <tr
-      className={`${styles.runRow} ${runIndex === elementIndex ? styles.selected : ''}`}
-      key={run.start_date_local}
-      onClick={handleClick}
-    >
-      {/*<td>{titleForRun(run)}</td>*/}
-	  <td>{run.name}</td>
-	  {/*heartRate && heartRate.toFixed(0)*/}
-	  <td>{run.type}</td>
-	  <td>{distance}</td>
-	  <td>{heartRate && heartRate.toFixed(0)}</td>
-      {paceParts && <td>{paceParts}</td>}
-      <td>{runTime}</td>
-      <td className={styles.runDate}>{run.start_date_local}</td>
-    </tr>
-  );
+    return (
+        <tr
+            className={`${styles.runRow} ${runIndex === elementIndex ? styles.selected : ''}`}
+            key={run.start_date_local}
+            onClick={handleClick}
+        >
+            {/*<td>{titleForRun(run)}</td>*/}
+            <td>{run.name}</td>
+            {/*heartRate && heartRate.toFixed(0)*/}
+            <td>{run.type}</td>
+            <td>{distance}</td>
+            <td>{heartRate && heartRate.toFixed(0)}</td>
+            {paceParts && <td>{paceParts}</td>}
+            <td>{runTime}</td>
+            <td className={styles.runDate} style={{ textAlign: 'center' }}>
+                {run.start_date_local}
+            </td>
+        </tr>
+    );
 };
 
 export default RunRow;
