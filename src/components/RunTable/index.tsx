25e32b0d4131f26dca618028b12b0bce3cd8a51a
Gusty <a0985209465@gmail.com>
2025-08-09 23:20:44 +0800

    格式化更新

--- src/components/RunTable/index.tsx
+++ src/components/RunTable/index.tsx
@@ -81,7 +81,7 @@ const RunTable = ({
       <table className={styles.runTable} cellSpacing="0" cellPadding="0">
         <thead>
           <tr>
-            <th style={{textAlign: 'left'}}>Name</th>
+            <th style={{ textAlign: 'left' }}>Name</th>
             {Array.from(sortFuncMap.keys()).map((k) => (
               <th key={k} onClick={handleClick}>
                 {k}
